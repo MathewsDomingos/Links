71597682794cfe11b82dc38f17da709b6913772a
Mateus Domingos Felipe <67337603+MathewsDomingos@users.noreply.github.com>
2022-07-07 13:56:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function ThemeDarkBlue() {
+  var element = document.body;
+  element.classList.remove("dark-blue");
+  element.classList.remove("pink");
+  element.classList.remove("pink-dark");
+  element.classList.remove("purpure");
+  element.classList.remove("dark-black");
+  element.classList.add("dark-blue");
+}
+
+function ThemePink() {
+  var element = document.body;
+  element.classList.remove("dark-blue");
+  element.classList.remove("pink");
+  element.classList.remove("pink-dark");
+  element.classList.remove("purpure");
+  element.classList.remove("dark-black");
+  element.classList.toggle("pink");
+}
+
+function ThemePinkDark() {
+  var element = document.body;
+  element.classList.remove("dark-blue");
+  element.classList.remove("pink");
+  element.classList.remove("pink-dark");
+  element.classList.remove("purpure");
+  element.classList.remove("dark-black");
+  element.classList.toggle("pink-dark");
+}
+
+function ThemePurpute() {
+  var element = document.body;
+  element.classList.remove("dark-blue");
+  element.classList.remove("pink");
+  element.classList.remove("pink-dark");
+  element.classList.remove("purpure");
+  element.classList.remove("dark-black");
+  element.classList.toggle("purpure");
+}
+
+function ThemeDarkBlack() {
+  var element = document.body;
+  element.classList.remove("dark-blue");
+  element.classList.remove("pink");
+  element.classList.remove("pink-dark");
+  element.classList.remove("purpure");
+  element.classList.remove("dark-black");
+  element.classList.toggle("dark-black");
+}
+
